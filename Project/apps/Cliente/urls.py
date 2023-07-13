68ebd4c75c2a4978e5ff8fd8a6dbd27814e38ebb
Ruffos1 <lando330055@gmail.com>
2023-07-12 21:58:56 -0300

    Forms creado

--- Project/apps/Cliente/urls.py
+++ Project/apps/Cliente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, crear_cliente
 
 app_name = "Cliente"
 
 urlpatterns = [
     path('',index, name="clientes" ),
+    path("crear_cliente", crear_cliente, name="crear_cliente")
 ]
--- Project/apps/Cliente/views.py
+++ Project/apps/Cliente/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Cliente
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from datetime import date
+from.models import Cliente
+from .forms import ClienteForm
 
 # Create your views here.
 def index(request):
@@ -7,3 +10,15 @@ def index(request):
     """Esta funcion muestra los objetos de un modelo"""
     contexto = {"Clientes": clientes_registros}
     return render(request, "cliente/index.html", context=contexto)
+
+def crear_cliente(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        #Indiaca que esta variable debe guardarse
+        if form.is_valid():
+            form.save()
+            return redirect("Cliente:clientes")
+    else: #request.method == "GET"
+        form = ClienteForm()
+
+    return render(request, "cliente/crear_clientes.html", context= {"form": form})
